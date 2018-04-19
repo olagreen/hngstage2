935721741606442f10d4485d96dc9e3ed80744ce
olagreen <38541675+olagreen@users.noreply.github.com>
2018-04-19 23:36:46 +0100

    Add files via upload

---
+++ hng4.php
@@ -0,0 +1,22 @@
+<! DOCTYPE html>
+<html>
+	<head>
+		<title>HNG INTERNSHIP 4.0</title>
+		<link href="https://fonts.googleapis.com/css?family=Amatic+SC|Raleway:100,200,600,700" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="content">
+			<figure>
+				<div style="text-align: right"><img src="ayo.jpg" width="50" height="50">
+				<figcaption>Ayo</figcaption></div>
+			</figure>
+			<h1> &nbsp | HNG INTERNSHIP 4.0</H1>
+		</div>
+		<div class="boxed">
+			<?php echo "<h2> " 	. date("h:i:sa")?></h1>
+			<?php echo "<h2> " 	. date("Y-m-d")?></h1>
+		</div>
+
+	</body>
+</html>
